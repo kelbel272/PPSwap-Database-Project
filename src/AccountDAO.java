d2c83a6254061ac1cc2b3372f9dd11b77d1b0822
kelbel272 <69172048+kelbel272@users.noreply.github.com>
2022-04-26 14:08:58 -0400

    Update AccountDAO.java

--- src/AccountDAO.java
+++ src/AccountDAO.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 /**
  * Servlet implementation class Connect
+ studentId = Integer.parseInt(request.getParameter("StudentId"));
  */
 
 public class AccountDAO {     
